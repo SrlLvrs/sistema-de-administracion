078ec8e6bfac40abf11cda6b5bb45dfcad5bd773
Israel Olivares <iolivares0505@gmail.com>
2024-07-23 21:18:27 -0400

    agregar cliente ok

--- src/views/CopyToClipboard.vue
+++ src/views/CopyToClipboard.vue
@@ -14,8 +14,8 @@
         <div class="grid grid-cols-1">
             <h1 class="text-center p-4 m-0">Clientes</h1>
             <div class="flex justify-center">
-                <!-- Botón CREAR NUEVO CLIENTE -->
-                <CrearCliente />
+                <!-- Botón CREAR NUEVO SECTOR -->
+                <crearSectorModal />
             </div>
         </div>
     </div>
@@ -61,15 +61,21 @@
                     <td>
                         <button class="btn btn-outline btn-info">
                             <a :href="cliente.linkmaps" target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                                </svg>
-                            </a>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+                        </a>
+
                         </button>
+                        <!-- 
+                            <div>
+                                <button @click="copyToClipboard(cliente.linkmaps)">Copiar texto</button>
+                            </div>
+                            -->
                     </td>
                 </tr>
             </tbody>
@@ -80,7 +86,6 @@
 <script>
 //Para usar axios, primero hay que instalarlo usando: 'npm install axios'
 import axios from "axios";
-import CrearCliente from "../components/CrearCliente.vue";
 
 export default {
     //Nombre del componente
@@ -93,7 +98,31 @@ export default {
         };
     },
 
-    methods: {},
+    methods: {
+        async copyToClipboard(maps) {
+            //const text = "Este es el texto a copiar";
+
+            if (navigator.clipboard) {
+                try {
+                    await navigator.clipboard.writeText(maps);
+                    alert("Texto copiado al portapapeles usando CLIPBOARD!");
+                } catch (err) {
+                    console.error("Error al copiar el texto: ", err);
+                }
+            } else {
+                // Fallback para navegadores que no soportan navigator.clipboard
+                const textArea = document.createElement("textarea");
+                textArea.value = maps;
+                textArea.style.position = "absolute";
+                textArea.style.left = "-9999px";
+                document.body.appendChild(textArea);
+                textArea.select();
+                document.execCommand("copy");
+                document.body.removeChild(textArea);
+                alert("Texto copiado al portapapeles usando execCommand!");
+            }
+        }
+    },
 
     //Método para llamar a la API cuando se cree la instancia
     created() {
@@ -102,7 +131,5 @@ export default {
 
         axios.get(url).then((response) => (this.clientes = response.data));
     },
-
-    components: { CrearCliente },
 }
 </script>
