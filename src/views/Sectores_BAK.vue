78f3ad00d429a183a4c9a8b20292cb83f93566f1
Israel Olivares <iolivares0505@gmail.com>
2024-07-25 16:48:51 -0400

    filter sectores

--- src/views/Sectores_BAK.vue
+++ src/views/Sectores_BAK.vue
@@ -7,17 +7,6 @@
             <div class="flex justify-center">
                 <!-- Botón CREAR NUEVO SECTOR -->
                 <crearSectorModal />
-
-                <!-- INPUT FILTRAR -->
-                <label class="input input-bordered flex items-center gap-2 ml-2">
-                    <input v-model="filterText" type="text" class="grow" placeholder="Sector o comuna..." />
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="h-4 w-4 opacity-70">
-                        <path fill-rule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </label>
             </div>
         </div>
     </div>
@@ -42,27 +31,26 @@
             <tbody>
                 <!-- GET -->
                 <!-- el nombre del array es SECTORES, que debe ser el mismo que se define en DATA() RETURN -->
-                <!-- Los resultados deben recorrerse dentro del TR 
-                -->
-                <tr v-for="item in filteredItems" :key="item.id">
-                        <!-- 
+                <!-- Los resultados deben recorrerse dentro del TR -->
+                <tr v-for="(sector, index) in sectores" :key="sector.id">
+                    <!-- 
                         <th> {{ index }}</th>
                         <td> {{ sector.id }}</td>
                         -->
-                    <td> {{ item.nombreSector }}</td>
-                    <td> {{ item.comuna }}</td>
-                    <td> {{ item.diaReparto }}</td>
-                    <td> {{ item.orden }}</td>
+                    <td> {{ sector.nombreSector }}</td>
+                    <td> {{ sector.comuna }}</td>
+                    <td> {{ sector.diaReparto }}</td>
+                    <td> {{ sector.orden }}</td>
                     <td>
                         <!-- Botón EDITAR SECTOR -->
                         <editarSectorModal 
-                        :id="item.id" 
-                        :nombreSector="item.nombreSector" 
-                        :comuna="item.comuna" 
-                        :diareparto="item.diaReparto" 
-                        :orden="item.orden" />
+                        :id="sector.id" 
+                        :nombreSector="sector.nombreSector" 
+                        :comuna="sector.comuna" 
+                        :diareparto="sector.diaReparto" 
+                        :orden="sector.orden" />
                         <!-- Botón BORRAR SECTOR -->
-                        <eliminarSectorModal :id="item.id" />
+                        <eliminarSectorModal :id="sector.id" />
                     </td>
                 </tr>
             </tbody>
@@ -84,22 +72,8 @@ export default {
     data() {
         return {
             //Array para guardar datos de la API
-            items: [],
-            filterText: '',
+            sectores: [],
         };
-    },
-
-    computed: {
-        //Esta función filtra el array en base a el NOMBRE o la DIRECCION del cliente
-        filteredItems() {
-            return this.items.filter(item => {
-                return item.nombreSector.toLowerCase().includes(this.filterText.toLowerCase()) || //esta linea filtra texto
-                    item.comuna.toLowerCase().includes(this.filterText.toLowerCase());
-                //item.edad.toString().includes(this.filterText); //esta linea filtra INTs
-                //hay que añadir una de estas 2 tipos de lineas, para cada una de las columnas a filtrar
-
-            });
-        }
     },
 
     components: { eliminarSectorModal, crearSectorModal, editarSectorModal },
@@ -109,7 +83,7 @@ export default {
         //Variable con endpoint
         let url = "https://nuestrocampo.cl/api/sectores/read.php";
 
-        axios.get(url).then((response) => (this.items = response.data));
+        axios.get(url).then((response) => (this.sectores = response.data));
     },
 };
 </script>
