16f21a0aef5545d4dc582e9cf32d4855fc90d59b
Israel Olivares <iolivares0505@gmail.com>
2024-08-03 17:55:02 -0400

    boton crear pedido con arrays y botones para añadir productos escogidos

--- src/components/CrearPedido.vue
+++ src/components/CrearPedido.vue
@@ -1,7 +1,6 @@
 <template>
     <!-- Modal CREAR NUEVO PEDIDO-->
     <!-- Botón para abrir el modal -->
-    <!-- ACÁ SE NECESITA UN ID UNICO PARA PODER ABRIR EL MODAL, SINO ABRIRÁ EL MODAL DE ELIMINAR CL -->
     <label :for="id" class="btn btn-outline btn-success mr-2" @click="crearPedido()">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="size-6">
@@ -13,14 +12,47 @@
     <input type="checkbox" :id="id" class="modal-toggle" />
     <div class="modal" role="dialog">
         <div class="modal-box">
-            <h3 class="text-lg font-bold">Nuevo Pedido</h3>
-            <p> ACÁ VA LA INFO DEL PEDIDO </p>
-            <p>El id del cliente es {{ cliente }}</p>
-            <p>{{ items }}</p>
-            <!-- ACÁ VA LA INFORMACION RELEVANTE DE ITEMS -->
+            <h3 class="text-lg font-bold text-center">Nuevo Pedido</h3>
+
+            <div v-for="item in items">
+                <!-- Nombre del cliente -->
+                <div class="label">
+                    <span class="label-text font-bold">Nombre del Cliente</span>
+                </div>
+                <p> {{ item.cliente }} </p>
+                
+                <!-- Estado del pedido -->
+                <div class="label">
+                    <span class="label-text font-bold">Estado</span>
+                </div>
+                <p> {{ item.estado }}</p>
+            </div>
+
+            <!-- Todos los productos -->
+            <div class="label">
+                <span class="label-text font-bold">Productos</span>
+            </div>
+
+            <select v-model="producto_seleccionado" class="select select-bordered w-full max-w-xs">
+                <option v-for="producto in productos">
+                    {{ producto.descripcion }}
+                </option>
+            </select>
+            <label class="btn" @click="agregarProducto()">Añadir</label>
+
+            <div class="label">
+                <span class="label-text font-bold">Productos elegidos</span>
+            </div>
+            <ul>
+                <li v-for="producto_elegido in productos_elegidos"> - {{ producto_elegido }}</li>
+            </ul>
+
+
+
+            <!-- Acciones -->
             <div class="modal-action">
                 <label :for="id" class="btn">Salir</label>
-                <button class="btn btn-outline btn-success" @click="editarSector(id)">
+                <button class="btn btn-outline btn-success" @click="">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -40,7 +72,7 @@ export default {
     name: "CrearPedidoModal",
 
     props: {
-        id: Number,
+        id: String,
         cliente: Number,
     },
 
@@ -48,6 +80,9 @@ export default {
         return {
             //Array para guardar datos de la API
             items: [],
+            productos: [],
+            producto_seleccionado: '',
+            productos_elegidos: [],
         };
     },
 
@@ -58,15 +93,26 @@ export default {
     methods: {
         crearPedido() {
             //POST nuevo pedido
-            let idc = this.cliente;
-            let url = `https://nuestrocampo.cl/api/pedidos/create.php?id_cliente=${idc}`
-            axios.post(url);
+            /**
+             * 
+             let idc = this.cliente;
+             let url = `https://nuestrocampo.cl/api/pedidos/create.php?id_cliente=${idc}`
+             axios.post(url);
+             */
 
             //GET último pedido
             let url2 = "https://nuestrocampo.cl/api/pedidos/last-read.php";
             setTimeout(() => {
                 axios.get(url2).then((response) => (this.items = response.data));
             }, 1000);
+
+            let url3 = "https://nuestrocampo.cl/api/productos/read.php";
+            axios.get(url3).then((response) => (this.productos = response.data));
+        },
+        agregarProducto(){
+            if (this.producto_seleccionado) {
+                this.productos_elegidos.push(this.producto_seleccionado)
+            }
         },
     }
 }
