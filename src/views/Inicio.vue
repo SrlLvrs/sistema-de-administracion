69b1614a00c8252d698d29d6d6d891998de99d2d
Israel Olivares <iolivares0505@gmail.com>
2024-09-13 20:11:22 -0300

    Inicio: Display de stats

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,9 +1,39 @@
 <template>
-    <div role="alert" class="alert alert-info">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-        </svg>
-        <span>Vista de inicio. Como el sistema aún está en construcción, algunos botones están en proceso. Por favor, no presionar ninguno.</span>
+    <!-- Estadísticas -->
+    <div class="stats shadow">
+        <div class="stat" v-for="item in items">
+            <div class="stat-title">{{ item.Nombre }}</div>
+            <div class="stat-value">{{ item.Valor }}</div>
+        </div>
     </div>
 </template>
+
+<script>
+//Para usar axios, primero hay que instalarlo usando: 'npm install axios'
+import axios from "axios";
+
+export default {
+    //Nombre del componente
+    name: "Inicio",
+
+    data() {
+        return {
+            items: [],
+        };
+    },
+
+    methods: {
+        //Nada por aqui...
+    },
+
+    computed: {
+        //Nada por acá...
+    },
+
+    //Método para llamar a la API cuando se cree la instancia
+    created() {
+        let url = "https://nuestrocampo.cl/api/inicio/read.php";
+        axios.get(url).then((response) => (this.items = response.data));
+    },
+}
+</script>
