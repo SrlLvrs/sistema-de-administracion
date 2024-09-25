00818c0da85970a270a90a6b61923f02a13cc958
Israel Olivares <iolivares0505@gmail.com>
2024-09-25 19:38:06 -0300

    Admin: Modal entregar pedido, transferencia y sin pagar

--- src/components/EntregarPedido.vue
+++ src/components/EntregarPedido.vue
@@ -15,6 +15,7 @@
 
             <p>Elige la opción que corresponda. Luego de eso, el pedido desaparecerá de la lista.</p>
 
+            <!-- Entregado, efectivo -->
             <div>
                 <label class="btn btn-outline btn-success m-2" @click="efectivo()">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -24,12 +25,24 @@
                     </svg>
                     Entregado, pagado con Efectivo</label>
             </div>
+            <!-- Entregado, transferencia -->
             <div>
-                <label class="btn btn-outline btn-success m-2" @click="pagarPedido()">Entregado, pagado con
-                    Transferencia</label>
+                <label class="btn btn-outline btn-success m-2" @click="transferencia()">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+                    </svg>
+                    Entregado, pagado con Transferencia</label>
             </div>
             <div>
-                <label class="btn btn-outline btn-warning m-2" @click="pagarPedido()">Entregado, sin pagar</label>
+                <label class="btn btn-outline btn-warning m-2" @click="entregado(id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                    </svg>
+                    Entregado, sin pagar</label>
             </div>
             <div>
                 <label class="btn btn-outline btn-error m-2" @click="pagarPedido()">Rechazado, el cliente explicitamente
@@ -41,6 +54,10 @@
             </div>
             <div>
                 <label class="btn btn-outline btn-error m-2" @click="pagarPedido()">Rechazado, otro motivo</label>
+            </div>
+            <!-- Acciones -->
+            <div class="modal-action ">
+                <label :for="label" class="btn">Salir</label>
             </div>
         </div>
     </div>
@@ -69,6 +86,14 @@ export default {
         pagarPedido() {
             //Nada
         },
+        async entregado(idp) {
+            //Marcar como Entregado
+            let url = `https://nuestrocampo.cl/api/pedidos/update-deliver.php?id=${idp}`
+            await axios.put(url).then(function (response) {
+                console.log(response.data)
+            })
+            location.reload()
+        },
         async efectivo() {
             //Pagado efectivo
             let idp = this.id
@@ -77,12 +102,17 @@ export default {
                 console.log(response.data)
             })
 
-            //Marcar como Entregado
-            let url2 = `https://nuestrocampo.cl/api/pedidos/update-deliver.php?id=${idp}`
-            axios.put(url2).then(function (response) {
+            await this.entregado(idp)
+        },
+        async transferencia() {
+            //pagado transferencia
+            let idp = this.id
+            let url = `https://nuestrocampo.cl/api/pedidos/pay-order-wire.php?id=${idp}`
+            await axios.put(url).then(function (response) {
                 console.log(response.data)
             })
-            location.reload()
+
+            await this.entregado(idp)
         }
     },
 };
