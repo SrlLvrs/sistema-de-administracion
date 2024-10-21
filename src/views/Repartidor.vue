b9fa87263d58db1ababab79f88ae7c618c0966f3
Israel Olivares <iolivares0505@gmail.com>
2024-10-21 08:38:27 -0300

    Repartidor: Quitar excel y telefono 2

--- src/views/Repartidor.vue
+++ src/views/Repartidor.vue
@@ -14,9 +14,6 @@
                             clip-rule="evenodd" />
                     </svg>
                 </label>
-
-                <!-- Botón Excel -->
-                <Excel :items="filteredItems" />
             </div>
         </div>
     </div>
@@ -72,25 +69,6 @@
                             </a>
                         </div>
                     </td>
-                    <td>
-                        <div class="flex flex-wrap gap-2">
-                            <a :href="'tel:' + item.Telefono2" class="btn btn-outline btn-warning">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                                </svg>
-                            </a>
-                            <a :href="'https://wa.me/' + item.Telefono2" target="_blank"
-                                class="btn btn-outline btn-success">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
-                                </svg>
-                            </a>
-                        </div>
-                    </td>
                     <td> {{ item.Pagado }}</td>
                     <td>
                         <!-- Acciones  -->
@@ -116,7 +94,6 @@ import axios from "axios";
 import EliminarPedido from "../components/EliminarPedido.vue";
 import DetallePedido from "../components/DetallesPedido.vue";
 import EditarPedido from "../components/EditarPedido.vue";
-import Excel from "../components/Excel.vue";
 import EntregarPedido from "../components/EntregarPedido.vue";
 
 export default {
@@ -179,7 +156,7 @@ export default {
         }
     },
 
-    components: { EliminarPedido, DetallePedido, EditarPedido, Excel, EntregarPedido},
+    components: { EliminarPedido, DetallePedido, EditarPedido, EntregarPedido},
 }
 </script>
 
